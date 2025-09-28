193a0facb9a8832c1a615cb88d3f4b4b2094c1be
mbayajames <jmuthiora244@gmail.com>
2025-09-28 02:02:10 -0700

    fix(routes): resolve issues in paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-
-router.post('/stkpush', paymentController.stkPush);
-router.post('/callback', paymentController.stkCallback);
-
-module.exports = router;
